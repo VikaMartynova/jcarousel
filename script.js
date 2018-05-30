328f6a8a1e5415df0ff5710d2a6b6615ecdc8783
Viktoria <vikam1210@gmail.com>
2018-05-30 19:22:53 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+(function($) {
+    $(function () {
+        $('.jcarousel').jcarousel({
+            wrap: 'circular',
+            vertical: true
+        });
+
+        $('.jcarousel-element').width($('.jcarousel').width());
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+    });
+})(jQuery);
+
+
